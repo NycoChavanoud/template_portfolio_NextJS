7815f47e6e07862510cbe9484305dbf946f88612
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-30 17:15:15 +0100

    step 5

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,38 @@
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 import Layout from "./layout";
 
 export default function Contact() {
-  return <Layout title="Contact"></Layout>;
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, mail);
+    setName("");
+    setMail("");
+  };
+
+  return (
+    <Layout title="Contact">
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="name"> Name </label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <label htmlFor="mail"> Mail </label>
+        <input
+          type="text"
+          placeholder="Email"
+          value={mail}
+          onChange={(e) => setMail(e.target.value)}
+        />
+        <button type="submit">SEND</button>
+      </form>
+    </Layout>
+  );
 }
